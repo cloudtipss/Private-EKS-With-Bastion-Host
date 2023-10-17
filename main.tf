e9484690d96f7cbd705e1a1e76b9f287236aeeac
Advissor <17081335+advissor@users.noreply.github.com>
2023-10-17 19:37:18 +0200

    V4 remove shell file; Move outputs

--- main.tf
+++ main.tf
@@ -179,9 +179,6 @@ data "external" "current_ipv4" {
   program = ["bash","-c","curl ipinfo.io"]
 }
 
-output "current_ipv4_json" {
-  value = data.external.current_ipv4.result["ip"]
-}
 
 
 module "eks" {
@@ -268,23 +265,3 @@ module "eks" {
   }
 }
 
-#############
-# module "eks_auth" {
-#   source = "aidanmelen/eks-auth/aws"
-#   eks    = module.eks
-
-#   map_roles = [
-#     {
-#       rolearn  = aws_iam_role.role.arn
-#       username = "testrole"
-#       groups = [
-#         "system:masters"
-#       ]
-#     },
-#   ]
-# }
-
-output "Connect_to_instance" {
-  description = "The public IP address assigned to the instance"
-  value       = "ssh -i ${var.path}/${var.private_key_name} ec2-user@${module.ec2-instance.public_ip}"
-}
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
+output "Connect_to_instance" {
+  description = "The public IP address assigned to the instance"
+  value       = "ssh -i ${var.path}/${var.private_key_name} ec2-user@${module.ec2-instance.public_ip}"
+}
+
 output "cluster_name" {
   description = "Name of the Cluster created"
   value       = module.eks.cluster_name
@@ -7,3 +12,8 @@ output "EC2_public_ip" {
   description = "The public IP address assigned to the instance"
   value       = module.ec2-instance.public_ip
 }
+
+output "current_ipv4_json" {
+  value = data.external.current_ipv4.result["ip"]
+}
+
